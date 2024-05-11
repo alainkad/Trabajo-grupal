6ae0bbc4a87ebefcd006bb6476c7c20dd29654eb
sanforyiu159 <75979929@certus.edu.pe>
2024-05-11 06:41:28 -0500

    juntando´proyecto

---
+++ inicio de pagina/css/link.js
@@ -0,0 +1,41 @@
+/*inicio usuario*/
+function formulario(){
+    location.href="http://127.0.0.1:5500/Trabajo-grupal/inicio%20de%20pagina/Formulario.html"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* planes - equipos- claro hogar...*/ 
+function redireccion(){
+    location.href="http://127.0.0.1:5500/Trabajo-grupal/inicio%20de%20pagina/equipos.html"
+}
+function redireccion2(){
+    location.href="http://127.0.0.1:5500/Trabajo-grupal/inicio%20de%20pagina/postpagomax.html"
+}
+function redireccion3(){
+    location.href="http://127.0.0.1:5500/Trabajo-grupal/inicio%20de%20pagina/clarohogar.html"
+}
+
+function redireccion4(){
+    location.href="http://127.0.0.1:5500/Trabajo-grupal/inicio%20de%20pagina/prepagochevere.html"
+}
+
